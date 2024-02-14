a6dc20cd201e317fde3adb77099542ca7a4ea298
mrpark219 <mrpark219@gmail.com>
2024-02-14 22:54:00 +0900

    🎉 실전! 스프링 부트와 JPA 활용 jpa-study 시작

---
+++ src/test/java/jpabook/jpastudy/JpaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook.jpastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
