9a5e8290c9350fe1575d99a2f0d0322d64e629a2
mrpark219 <mrpark219@gmail.com>
2024-02-19 00:38:07 +0900

    ✨ Item(상품) 엔티티에 비즈니스 로직 추가

--- src/main/java/jpabook/jpastudy/domain/item/Item.java
+++ src/main/java/jpabook/jpastudy/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpastudy.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpastudy.domain.Category;
+import jpabook.jpastudy.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -30,4 +31,28 @@ public abstract class Item {
 
 	@ManyToMany(mappedBy = "items")
 	private List<Category> categories = new ArrayList<>();
+
+	// === 비즈니스 로직 ===
+
+	/**
+	 * stock 증가
+	 */
+	public void addStock(int quantity) {
+
+		this.stockQuantity += quantity;
+	}
+
+	/**
+	 * stock 감소
+	 */
+	public void removeStock(int quantity) {
+
+		int restStock = this.stockQuantity - quantity;
+
+		if(restStock < 0) {
+			throw new NotEnoughStockException("need more stock");
+		}
+
+		this.stockQuantity = restStock;
+	}
 }
