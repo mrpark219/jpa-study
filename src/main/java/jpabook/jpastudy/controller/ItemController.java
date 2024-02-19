bbb3d1dc47daded37f3ae68b772a76e66723b3a1
mrpark219 <mrpark219@gmail.com>
2024-02-19 12:26:56 +0900

    ✨ 상품 목록 조회 기능 추가

--- src/main/java/jpabook/jpastudy/controller/ItemController.java
+++ src/main/java/jpabook/jpastudy/controller/ItemController.java
@@ -34,4 +34,10 @@ public class ItemController {
 
 		return "redirect:/";
 	}
+
+	@GetMapping("/items")
+	public String list(Model model) {
+		model.addAttribute("items", itemService.findItems());
+		return "items/itemList";
+	}
 }
