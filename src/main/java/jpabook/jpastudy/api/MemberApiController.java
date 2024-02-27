12866c067ff8145378fbbf212d5ef3755930cd41
mrpark219 <mrpark219@gmail.com>
2024-02-28 00:25:30 +0900

    ✨ 회원 수정 API 추가

--- src/main/java/jpabook/jpastudy/api/MemberApiController.java
+++ src/main/java/jpabook/jpastudy/api/MemberApiController.java
@@ -4,11 +4,10 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import jpabook.jpastudy.domain.Member;
 import jpabook.jpastudy.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -40,6 +39,29 @@ public class MemberApiController {
 
 		Long id = memberService.join(member);
 		return new CreateMemberResponse(id);
+	}
+
+	/**
+	 * 회원 수정 v2
+	 */
+	@PutMapping("/api/v2/members/{id}")
+	public UpdateMemberResponse updateMemberV2(@PathVariable Long id, @RequestBody @Valid UpdateMemberRequest request) {
+
+		memberService.update(id, request.getName());
+		Member findMember = memberService.findOne(id);
+		return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+	}
+
+	@Data
+	static class UpdateMemberRequest {
+		private String name;
+	}
+
+	@Data
+	@AllArgsConstructor
+	static class UpdateMemberResponse {
+		private Long id;
+		private String name;
 	}
 
 	@Data
