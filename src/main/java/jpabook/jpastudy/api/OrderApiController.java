b9c02629012e629dced9baeeb77e33469e94e513
mrpark219 <mrpark219@gmail.com>
2024-02-29 12:32:36 +0900

    ✨ 주문 조회 API v2 추가 - 엔티티를 DTO로 변환하여 노출

--- src/main/java/jpabook/jpastudy/api/OrderApiController.java
+++ src/main/java/jpabook/jpastudy/api/OrderApiController.java
@@ -1,14 +1,19 @@
 package jpabook.jpastudy.api;
 
+import jpabook.jpastudy.domain.Address;
 import jpabook.jpastudy.domain.Order;
 import jpabook.jpastudy.domain.OrderItem;
+import jpabook.jpastudy.domain.OrderStatus;
 import jpabook.jpastudy.repository.OrderRepository;
 import jpabook.jpastudy.repository.OrderSearch;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,5 +34,52 @@ public class OrderApiController {
 		}
 
 		return all;
+	}
+
+	@GetMapping("/api/v2/orders")
+	public List<OrderDto> ordersV2() {
+
+		List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+
+		return orders.stream()
+			.map(OrderDto::new)
+			.collect(Collectors.toList());
+	}
+
+	@Getter
+	static class OrderDto {
+
+		private Long orderId;
+		private String name;
+		private LocalDateTime orderDate;
+		private OrderStatus orderStatus;
+		private Address address;
+		private List<OrderItemDto> orderItems;
+
+		public OrderDto(Order order) {
+			orderId = order.getId();
+			name = order.getMember().getName();
+			orderDate = order.getOrderDate();
+			orderStatus = order.getStatus();
+			address = order.getDelivery().getAddress();
+			order.getOrderItems().forEach(orderItem -> orderItem.getItem().getName());
+			orderItems = order.getOrderItems().stream()
+				.map(orderItem -> new OrderItemDto(orderItem))
+				.collect(Collectors.toList());
+		}
+	}
+
+	@Getter
+	static class OrderItemDto {
+
+		private String itemName;
+		private int orderPrice;
+		private int count;
+
+		public OrderItemDto(OrderItem orderItem) {
+			itemName = orderItem.getItem().getName();
+			orderPrice = orderItem.getOrderPrice();
+			count = orderItem.getCount();
+		}
 	}
 }
